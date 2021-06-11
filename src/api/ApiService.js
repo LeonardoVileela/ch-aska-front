1c6c5fc9f6f37e816d52544fd4720073a7f604ba
Yuumi <leovilela.empresa@gmail.com>
2021-06-11 16:13:49 -0400

    version 1.1.0

--- src/api/ApiService.js
+++ src/api/ApiService.js
@@ -151,25 +151,6 @@ class ApiService {
 
     //Statistics
 
-    load(id, onLoad, onError) {
-        axios.get(`${API_ENDPOINT}/tasks/${id}`, this.buildAuthHeader())
-            .then(response => onLoad(response.data))
-            .catch(e => onError(e));
-    }
-
-    save(task, onSave, onError) {
-        if (task.id === 0) {
-            axios.post(`${API_ENDPOINT}/tasks`, task, this.buildAuthHeader())
-                .then(() => onSave())
-                .catch(e => onError(e));
-
-        } else {
-            axios.put(`${API_ENDPOINT}/tasks/${task.id}`, task, this.buildAuthHeader())
-                .then(() => onSave())
-                .catch(e => onError(e));
-        }
-    }
-
     buildAuthHeader() {
         return {
             headers: {
